0143803458e27ab6faf6bce17fbb69e4109649b6
Natali Lohr <nlohr@lohrconstructionco.com>
2022-09-13 22:18:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { data } from './data';
+
 
 function App() {
+
+  const [dress, setDress] = useState(0);
+  const {id, dressName, description, image, brand, price, rate} = data[dress];
+
+  const previousDress = () => {
+    setDress ((dress=> {
+      dress--;
+      if (dress<0) {
+        return data.length -1;
+      }
+      return dress;
+    }))
+  }
+
+  const nextDress = () => {
+    setDress ((dress=> {
+      dress++;
+      if (dress> data.length -1) {
+        dress=0;
+      }
+      return dress;
+    }))
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>Bestsellers in 2022</h1>
+      </div>
+      
+      <div className='container two' key={id}>
+
+        <div className='boxOne'>
+          <img src={image} alt="dress" width="300px"/>
+        </div>
+
+        <div className='boxTwo'>
+          <div>
+            <h2>{dressName}</h2>
+          </div>
+          
+          <div>
+            <p>{rate}</p>
+          </div>
+
+          <div>
+            <p>Brand: 💖 <b>{brand}</b></p>
+          </div>
+
+          <div>
+            <p>{description}</p>
+          </div>
+
+          <div>
+            <p className='price'>{price}</p>
+          </div>
+
+          <div className='container three'>
+        <button onClick={previousDress}>🠠 Previous</button>
+        <button onClick={nextDress}>Next 🠢</button>
+      </div>
+        </div>
+
+      </div>
+
+      
+
+
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const data = [
+    {id: 1,  
+        dressName: "Evening of Elegance Navy Blue Floral Jacquard Wrap Midi Dress",  
+        description: "Lightweight woven fabric, with a floral jacquard pattern, shapes this fabulous dress that has a surplice neckline and sheer long sleeves with button cuffs. Adjustable wrap silhouette, with tying waist sash and hidden internal ties, ends at a slightly ruffled midi hem.",  
+        image: "https://www.lulus.com/images/product/xlarge/5223770_1045422.jpg?w=560&hdpi=1",  
+        brand: "Lulus", 
+        price: "$89",
+        rate: "⭐⭐⭐⭐⭐"
+    },
+
+    {id: 2,  
+        dressName: "Total Appeal Black Ruched One-Shoulder Bodycon Midi Dress",  
+        description: "Slinky jersey knit sweeps across a one-shoulder neckline and a single tank strap, and into a figure-skimming bodice. The curve-hugging fit continues down to the asymmetrical hem, while ultra-sexy ruching adds a little something extra to the sides.",  
+        image: "https://www.lulus.com/images/product/xlarge/7928941_880562.jpg?w=560&hdpi=1",  
+        brand: "Lulus", 
+        price: "$62",
+        rate: "⭐⭐⭐⭐"
+    },
+    {id: 3,  
+        dressName: "Shine Language Rose Gold Sequin Mermaid Maxi Dress",  
+        description: "Sheer mesh, embellished with an eye-catching display of rose gold sequins, covers this stunning gown with adjustable spaghetti straps, a V-neckline, and a princess-seamed bodice. High, empire waist flows into a figure-flaunting maxi dress with a picture-perfect mermaid hem. Hidden back zipper/clasp.",  
+        image: "https://www.lulus.com/images/product/xlarge/5344530_1017882.jpg?w=560&hdpi=1",  
+        brand: "Lulus", 
+        price: "$69",
+        rate: "⭐⭐⭐⭐⭐"
+    },
+    {id: 4,  
+        dressName: "Forever Your Girl Slate Blue Satin Bodycon Dress",  
+        description: "Chic stretch knit with a satin sheen falls from skinny straps into a V-neck, princess seamed bodice (with elasticized back), and a bodycon wrap skirt that is sure to flaunt your figure.",  
+        image: "https://www.lulus.com/images/product/xlarge/4908450_1002502.jpg?w=560&hdpi=1",  
+        brand: "Lulus", 
+        price: "$48",
+        rate: "⭐⭐⭐⭐⭐"
+    },
+    {id: 5,  
+        dressName: "Moments Of Bliss Rust Red Backless Mermaid Maxi Dress",  
+        description: "Stretchy crepe stretch knit shapes this stunning maxi dress with thin straps, a V-neckline, and darted bodice with a wide back cutout, secured by a triple-button closure. High, fitted waist flows into a figure-flaunting mermaid maxi skirt. Hidden back zipper/clasp.",  
+        image: "https://www.lulus.com/images/product/xlarge/7999641_1618796.jpg?w=560&hdpi=1",  
+        brand: "Lulus", 
+        price: "$89",
+        rate: "⭐⭐⭐⭐"
+    },
+
+    {id: 6,  
+        dressName: "Heart of Marigold Navy Blue Floral Print Wrap Maxi Dress",  
+        description: "White floral print embellishes breezy woven rayon as it drapes into a sultry surplice bodice, framed by fluttering short sleeves. Wrapping maxi skirt secures via two adjustable ties at the waist.",  
+        image: "https://www.lulus.com/images/product/xlarge/3074630_453272.jpg?w=560&hdpi=1",  
+        brand: "Lulus", 
+        price: "$79",
+        rate: "⭐⭐⭐⭐⭐"
+    }
+]
